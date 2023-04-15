07b2ec1a36e60a9cd8c7dabaa8674c398f802b81
david <davidcalieln@gmail.com>
2023-04-14 20:07:17 -0700

    Add basic Arena example

--- src/arena.rs
+++ src/arena.rs
@@ -2,6 +2,21 @@ use crate::raw_arena::RawArena;
 use core::ptr::NonNull;
 
 /// An arena, owns regions of memory that objects are bump allocated into.
+///
+/// # Example
+///
+/// ```
+/// use bumpercar::prelude::*;
+///
+/// let arena = Arena::new();
+/// let my_num = arena.alloc(5i32);
+/// assert_eq!(*my_num, 5i32);
+///
+/// arena.alloc("hello");
+///
+/// *my_num = 0xABCDi32;
+/// assert_eq!(*my_num, 0xABCDi32);
+/// ```
 pub struct Arena {
     arena: RawArena,
 }
