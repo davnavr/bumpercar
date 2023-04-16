a8438fa5e4d42e18143320d6be64aac195de4a04
david <davidcalieln@gmail.com>
2023-04-16 12:33:57 -0700

    Add Arena::reset

--- src/arena.rs
+++ src/arena.rs
@@ -28,6 +28,16 @@ impl Arena {
     pub fn allocator(&mut self) -> crate::Allocator<'_> {
         crate::Allocator::with_arena(&mut self.arena)
     }
+
+    /// Resets the arena by moving the bump pointer back to the first chunk.
+    ///
+    /// This allows reusing of memory allocated by the arena.
+    pub fn reset(&mut self) {
+        unsafe {
+            // Safety: &mut self ensures there are no extant references that can become dangling
+            self.arena.reset()
+        }
+    }
 }
 
 impl core::default::Default for Arena {
